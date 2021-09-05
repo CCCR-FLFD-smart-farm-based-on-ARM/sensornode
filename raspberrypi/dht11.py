ec1e72df3a624000640c04bef9b84ffd60b88d13
ndiepray <86943561+ndiepray@users.noreply.github.com>
2021-09-05 22:39:40 +0900

    Update dht11.py

--- raspberrypi/dht11.py
+++ raspberrypi/dht11.py
@@ -1,1 +1,23 @@
+import time
+import Adafruit_DHT
 
+# Sensor는 DH11 사용, pin은 GPIO Number 4번
+sensor = Adafruit_DHT.DHT11
+pin = 4
+
+# 1초마다 temperature data, humidity data 출력
+try:
+    while True :
+        # t: temperature, h: humidity
+        h, t = Adafruit_DHT.read_retry(sensor, pin)
+        if h is not None and t is not None :
+            print("Temperature = {0:0.1f}*C Humidity = {1:0.1f}%".format(t, h))
+        else :
+            print('Read error')
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print("Terminated by Keyboard")   
+
+finally:
+    print("End of Program")
